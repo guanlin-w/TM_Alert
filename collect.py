f6363c8755192e5883ec3347c89df1243272e633
guanlin-w <36206304+guanlin-w@users.noreply.github.com>
2025-01-08 22:32:41 -0600

    collect script now clicks through the button and filters for the desired ticket amount

--- collect.py
+++ collect.py
@@ -1,28 +1,58 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from config import CommonConfig
+from selenium.webdriver.support.ui import Select
+from config import UserConfig
 import time
 
 # Gathers the data
 
 # set up the webdriver 
 options= Options()
-options.add_argument("--headless=new")
+#options.add_argument("--headless=new")
 driver = webdriver.Chrome(options=options)
-driver.get(CommonConfig.url)
+# TODO add check to ensure that the url is proper
+driver.get(UserConfig.url)
 
-# load the page
+# let the page elements to load
 time.sleep(5)
 
-# elements = driver.find_elements(By.TAG_NAME,"button")
-# for e in elements:
-#     if e.text == CommonConfig.popupButtonText:
-#         print(e.get_attribute(CommonConfig.popupButtonAttr))
-    # popupButtonAttr = "data-bdd"
-    # popupButtonValue = "accept-modal-accept-button"
-element = driver.find_element(By.CSS_SELECTOR, f"button[{CommonConfig.popupButtonAttr}={CommonConfig.popupButtonValue}]")
-print(element.text)
+
+
+popupButton = None
+# click through the popup
+try:
+    popupButton = driver.find_element(By.CSS_SELECTOR, f"button[{UserConfig.popupButtonAttr}={UserConfig.popupButtonValue}]").click()
+    print("Button Clicked")
+except:
+    # TODO: error handle this
+    # should probably keep track of this in case the popup doesn't show up
+    print("LOL SHOULDN't Happen :3")
+
+
+# filter by the number of tickets 
+
+select = None
+try:
+    dropdown = driver.find_element(By.CSS_SELECTOR, f"select[id={UserConfig.ticketDropdownId}]")
+    print(dropdown.text)
+    select = Select(dropdown)
+    
+except:
+    print("FAILED TO FILTER")
+
+
+# Try to filter if possible
+if select is not None:
+    try:
+        select.select_by_visible_text(UserConfig.ticketDesiredAmountFilter)
+    except:
+        print("Failed to filter by specified amount")
+
+        
+
+
+
 # with open("temp.txt", "w", encoding="utf-8") as f:
 #     f.write(driver.page_source)
-driver.close()
+#driver.close()
--- config.py
+++ config.py
@@ -2,11 +2,24 @@
 # contains the url to scrap from
 
 class CommonConfig(object):
-    url = "https://www.ticketmaster.com/hans-zimmer-live-austin-texas-01-31-2025/event/3A00616BCA113C1E" # Change to evaluate a different page
-    
+    # Configs related to scraping the data
+    # Change this if the scraping job is failing
+
     # used to identify the accept and continue popup
     popupButtonText = "Accept & Continue"
     popupButtonAttr = "data-bdd"
     popupButtonValue = "accept-modal-accept-button"
-class TestConfig(CommonConfig):
-    url = "meme.txt"
+
+
+    # used to filter by number of tickets
+    ticketDropdownId = "filter-bar-quantity"
+    
+    
+
+class UserConfig(CommonConfig):
+    # Configs based on the user's preference
+    # Change this if you are looking for different elements
+    url = "https://www.ticketmaster.com/hans-zimmer-live-austin-texas-01-31-2025/event/3A00616BCA113C1E" # Change to evaluate a different page
+    ticketDesiredAmount = 1
+    
+    ticketDesiredAmountFilter = "1 Ticket" if ticketDesiredAmount == 1 else f"{ticketDesiredAmount} Tickets"
